7b98f78b68f78a28834c50ba7dd8a0c386f9b914
本本本本本本 <94464399+LinearBen@users.noreply.github.com>
2025-08-03 20:46:24 +0800

    Add Spring MVC Hibernate example

---
+++ src/main/java/com/example/dao/UserDaoImpl.java
@@ -0,0 +1,28 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void save(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    public List<User> list() {
+        return sessionFactory.getCurrentSession()
+                .createQuery("from User", User.class)
+                .list();
+    }
+}
